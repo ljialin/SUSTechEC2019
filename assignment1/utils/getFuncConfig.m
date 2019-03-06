0be665a1afc7b18b41267abef167d3eb8a59ccf3
Jialin Liu <ljialin@users.noreply.github.com>
2019-03-06 10:47:53 +0800

    correct budget
    Correct the generation number and budget (evaluation number)

--- assignment1/utils/getFuncConfig.m
+++ assignment1/utils/getFuncConfig.m
@@ -1,11 +1,15 @@
 function [funcName,config,budget] = getFuncConfig(funcIdx)
+% REFERENCE: 
+% Yao, Xin, Yong Liu, and Guangming Lin. "Evolutionary programming made 
+% faster." IEEE Transactions on Evolutionary computation 3.2 (1999): 82-102.
 %
 % Author: Jialin Liu
 % email: jialin.liu.cn@gmail.com
 % Website: http://www.liujialin.tech/
 % Feb 2019; Last revision: 20-Feb-2019
 %
-% benchmark function names
+% Benchmark function names
+% (fucntion indices in the reference: 7, 8, 10, 11, 18, 21)
 benchmarkNames={'step','noisyQuartic','rastrigin','ackley', ...
     'goldsteinPrice','shekel'};
 % Each row refers to the configuration of a fitness function
@@ -17,11 +21,13 @@ funcConfig=[...
     30, -32, 32;...
     2, -2, 2;...
     4, 0, 10];
-% Budegt for different benchmark functions
-generations=[1400, 2000, 5000, 5000, 20000, 1400, 3000];
+% Nb of generations for different benchmark functions in the reference (population size=100)
+generations=[3000, 2000, 1500, 2000, 100, 100];
+% Budget for different benchmark functions
+evaluations=100.*generations;
 
 funcName=benchmarkNames{funcIdx};
 config=funcConfig(funcIdx,:)
-budget=generations(funcIdx);
+budget=evaluations(funcIdx);
 
 end
